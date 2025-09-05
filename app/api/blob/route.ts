a49d7ba1581ab435097a600b9e2d577dacde06ef
byte2069 <webdev997@gmail.com>
2025-09-06 01:35:31 +0700

    2

--- app/api/blob/route.ts
+++ app/api/blob/route.ts
@@ -1,4 +1,3 @@
-// app/api/blob/route.ts
 import { generateClientToken } from "@vercel/blob";
 
 export const runtime = "edge";
@@ -6,7 +5,7 @@ export const runtime = "edge";
 export async function GET() {
   const token = await generateClientToken({
     allowedContentTypes: ["image/*"],
-    maximumSizeInBytes: 15 * 1024 * 1024,
+    maximumSizeInBytes: 15 * 1024 * 1024, // 15MB
     access: "public",
   });
   return Response.json(token);
